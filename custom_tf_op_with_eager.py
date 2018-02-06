de1245d09caf4bf28a27eb0ed3d3966e19522432
safooray <safoora.yousefi1192@gmail.com>
2018-02-06 15:38:12 -0500

    custom gradient with tf eager

---
+++ custom_tf_op_with_eager.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import histogram_ops
+import tensorflow.contrib.eager as tfe
+
+
+PROOF = 1
+OP_NAME='my_op'
+
+@tfe.custom_gradient
+def python_func(x_in):
+    def grad_func(grad):
+        # Return custom gradient wrt each input of the op.
+        return grad * ((2 * tf.exp(x_in)) - 1) + PROOF
+
+    forward_func = tf.subtract(2 * tf.exp(x_in), x_in)
+    return forward_func, grad_func
+
+if __name__=='__main__':
+    with tf.Graph().as_default():
+        x = tf.constant([[2.0, 4.0, 6.0], [6.0, 8.0, 10.0]], tf.float32, (2, 3),
+                        'input')
+        expected_op = tf.subtract(2 * tf.exp(x), x)
+        expected_gr = tf.gradients(expected_op, [x])
+
+        actual_op = python_func(x)
+        actual_gr = tf.gradients(actual_op, [x])
+
+        with tf.Session() as sess:
+            tf.global_variables_initializer().run()
+            actual_gr_val, expected_gr_val = sess.run([actual_gr, expected_gr])
+            np.testing.assert_array_equal(expected_gr_val,
+                                          [a - PROOF for a in actual_gr_val])
